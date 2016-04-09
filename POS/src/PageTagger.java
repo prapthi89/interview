ac2ddd7b62d81da074f05427ccd681aaa54b3e8e
prapthih <prapthih@usc.edu>
2016-04-08 17:11:39 -0700

    added comments

--- POS/src/PageTagger.java
+++ POS/src/PageTagger.java
@@ -6,77 +6,74 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
- 
-public class PageTagger {
-	public String tagText(String in) {
-		MaxentTagger tagger = new MaxentTagger(
-                "taggers/english-left3words-distsim.tagger");
- 
-        // The tagged string
-        return tagger.tagString(in);
+
+public class PageTagger 
+{
+	private MaxentTagger tagger;
+	public PageTagger()
+	{
+		tagger = new MaxentTagger("taggers/english-left3words-distsim.tagger");
 	}
-	public String getText(URL url) {
+	public String tagText(String in) 
+	{
+		return tagger.tagString(in);
+	}
+	
+	public String getText(URL url) 
+	{
 		String urlString = "";
-		try {
+		try 
+		{
 			URLConnection conn = url.openConnection();
 			HttpURLConnection connection = null;
-	        connection = (HttpURLConnection) conn;
-	        Boolean body=false;
-	        BufferedReader in = new BufferedReader(
-	                new InputStreamReader(connection.getInputStream()));
-	                String current;
-	                while((current = in.readLine()) != null)
-	                {
-	                	if(current.contains("body"))
-	                		body=true;
-	                		
-	                   if(body)
-	                   urlString += current;
-	                }
-	                //using Jsoup, which is how html parsing is done
-	                Document doc = Jsoup.parse(urlString);
-	                return (doc.text());
-	                
-	                /*Pattern pattern = Pattern.compile("(<style[^>]*>([\\s\\S]*?)<\\/style>)");
-	                Matcher matcher = pattern.matcher(urlString);
-	                
-	            
-	                	urlString = matcher.replaceAll("");
-	                
-	                //to replace all script tags and it's contents by ""
-	                pattern = Pattern.compile("(<script[^>]*>([\\s\\S]*?)<\\/script>)");
-	                matcher = pattern.matcher(urlString);
-	                urlString = matcher.replaceAll("");pattern = Pattern.compile("<.*?>");
-	                matcher = pattern.matcher(urlString);urlString = matcher.replaceAll("");
-	                return urlString;*/ 
-	        
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			connection = (HttpURLConnection) conn;
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String current;
+			while((current = in.readLine()) != null)
+			{
+				urlString += current;
+			}
+			urlString = Jsoup.parse(urlString).text();
+		} 
+		catch (IOException e) 
+		{
 			e.printStackTrace();
-		}	
-		 return urlString;
+		}
+		return urlString;
 	}
-    public static void main(String[] args) throws IOException,
-            ClassNotFoundException {
- 
-    	String input = args[0];
-    	URL url = new URL(input); 
-        PageTagger pagetag = new PageTagger();
-        String res1, res2;
-        try {
-        	res1 = pagetag.getText(url);
-        	res2 = pagetag.tagText(res1);
-        	PrintWriter writer = new PrintWriter("/home/prapthi/workspace_java/POS/output.txt", "UTF-8");
-        	writer.println("Tagged text of "+input+" is:");
-        	writer.println(res2);
-        	writer.close();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-    }
+	public static void main(String[] args) throws IOException,
+	ClassNotFoundException 
+	{
+
+		URL url;
+		PageTagger tagger = new PageTagger();
+		PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
+		
+		for(int i=0;i<args.length;i++)
+		{
+			url = new URL(args[i]);
+			writer.println("************** POS Tagged For "+url.getHost()+" *****************\n");
+			//text has the text of the url
+			String text = tagger.getText(url);
+			//splitting text in to chunks of size 1000, and tagging it using tagText
+			String[] textChunks = text.split(" ");
+			long size = textChunks.length;
+			int chunkSize = 1000;
+			int j = 0;
+			while(j<size)
+			{
+				String subString = "";
+				for(int k=0;k<chunkSize && j<size;k++,j++)
+				{
+					subString = subString+" "+ textChunks[j];
+				}
+				writer.println(tagger.tagText(subString));
+			}
+			writer.println();
+			writer.println("\n********************************************\n\n");
+		}
+		
+		writer.close();
+	}
 }
